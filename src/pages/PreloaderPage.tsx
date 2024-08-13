0f5814e7b24567f4e94d689f2df77bb8c7d1bea2
DMProductionn <goluzin.dima008@mail.ru>
2024-08-13 13:41:02 +0500

    dd

--- src/pages/PreloaderPage.tsx
+++ src/pages/PreloaderPage.tsx
@@ -1,11 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import Preloader from '../shared/Preloader';
 
 
 const PreloaderPage = () => {
+  const navigate = useNavigate();
 
 
   return (
+    <>
+    <button onClick={() => navigate('/deals')}>клик</button>
     <Preloader />
+    </>
   );
 };
 
