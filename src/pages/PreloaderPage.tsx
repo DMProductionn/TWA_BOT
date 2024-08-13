83576ab4733265fef7f8b4b1808ff8843466ece5
DMProductionn <goluzin.dima008@mail.ru>
2024-08-13 13:33:05 +0500

    d

--- src/pages/PreloaderPage.tsx
+++ src/pages/PreloaderPage.tsx
@@ -7,7 +7,10 @@ const PreloaderPage = () => {
 
   useEffect(() => {
    const test = localStorage.getItem('user_auth')
+   const test2 = localStorage.getItem('dc3_server_salt')
    console.log(test);
+   console.log(test2);
+   
    
   }, [])
 
