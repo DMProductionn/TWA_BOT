f7337e72bf75cf0b946ee7db756c65e51441f147
DMProductionn <goluzin.dima008@mail.ru>
2024-08-13 17:04:54 +0500

    dd

--- src/pages/PreloaderPage.tsx
+++ src/pages/PreloaderPage.tsx
@@ -1,19 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Preloader from '../shared/Preloader';
 import axios from 'axios';
 
 
 const PreloaderPage = () => {
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    const test = async () => {
-      const data = await axios.post('https://paymentfreebot.onrender.com/auth/webhook')
-      console.log(data);
-    }
+    const ws = new WebSocket('ws://https://paymentfreebot.onrender.com/auth/webhook');
 
-    test();
-  }, [])
-  
+    ws.onmessage = (event) => {
+        const newData = JSON.parse(event.data);
+        console.log('Received data from websocket:', newData);
+        setData(newData);
+    };
+
+    console.log(data);
+    
+
+    return () => {
+        ws.close();
+    };
+}, []);
+
 
   return (
     <>
