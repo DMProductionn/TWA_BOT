282bb84b8110fd4e20dcb693165060c0d8e3c535
DMProductionn <goluzin.dima008@mail.ru>
2024-08-13 17:16:14 +0500

    ss

--- src/pages/PreloaderPage.tsx
+++ src/pages/PreloaderPage.tsx
@@ -6,12 +6,13 @@ const PreloaderPage = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const ws = new WebSocket('https://paymentfreebot.onrender.com/auth/webhook');
+    const ws = new WebSocket('wss://https://paymentfreebot.onrender.com/auth/webhook');
 
     ws.onmessage = (event) => {
         const newData = JSON.parse(event.data);
         console.log('Received data from websocket:', newData);
         setData(newData);
+        console.log(data);
     };
 
     console.log(data);
