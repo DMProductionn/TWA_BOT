1a2590d0cb54afcd186294d24c57441fabbb6951
DMProductionn <goluzin.dima008@mail.ru>
2024-10-31 12:37:22 +0500

    ggg

--- src/app/services/token.service.ts
+++
@@ -1,8 +0,0 @@
-import https from "../Api/https"
-
-
-// export const addToken = async (chat_id: {chat_id: number}) => {
-//   const res = await https.post('/auth/create/token', chat_id);
-//   localStorage.setItem('token', res.data.token);
-//   return res.data.token;
-// }
