832e17878ce9b2d4f6afc0577aa65dfedfe25eb4
DMProductionn <goluzin.dima008@mail.ru>
2024-10-31 13:48:30 +0500

    ddd

--- src/app/Routes/App.tsx
+++ src/app/Routes/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 import History from '../../pages/History';
 import Profile from '../../pages/Profile';
@@ -8,15 +8,12 @@ import HistoryBalance from '../../pages/HistoryBalance';
 
 function App() {
 
-  const token = localStorage.getItem('token');
-
   return (
     <>
       {
         <Routes>
           <Route index path="/token/:token" element={<PreloaderPage />} />
           <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Navigate to={`/token/${token}`} />} />
             <Route path="/deals" element={<Deals />} />
             <Route path="/history" element={<History />} />
             <Route path="/profile" element={<Profile />} />
