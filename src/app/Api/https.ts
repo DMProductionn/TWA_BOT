b7af8ec1fa5402e2daf64e841d3f39e2111e26d1
DMProductionn <goluzin.dima008@mail.ru>
2024-10-31 13:37:00 +0500

    вввв

--- src/app/Api/https.ts
+++ src/app/Api/https.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const https = axios.create({
-  baseURL: 'http://localhost:8000', 
+  baseURL: 'https://paymentfreebot.onrender.com', 
   headers: {
-    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5Njg2NTg1NTQifQ.P57B4IgT6OVPYfwgT2apu7B6B2TFW_5i31glrKjXHRw`
+    'Authorization': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5Njg2NTg1NTQifQ.P57B4IgT6OVPYfwgT2apu7B6B2TFW_5i31glrKjXHRw`
   }
 })
 
