dc825b8530d51369c547b26e691a809aef791cf7
DMProductionn <goluzin.dima008@mail.ru>
2025-03-21 20:49:24 +0500

    ldld

--- src/app/Api/https.ts
+++ src/app/Api/https.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const https = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://paymentfreebot-telegram-web-bot.onrender.com/',
 });
 
 https.interceptors.request.use(
